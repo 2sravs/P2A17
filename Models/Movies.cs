d820a3392e5b99394cb25c23b4f6d24329dad1f2
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-04 15:04:34 +0530

    firstcommit

---
+++ Models/Movies.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPIMovies.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+    }
+}
